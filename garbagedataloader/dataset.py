47a854a716b667b65cc1fd9f9f766296d2de99ba
Henry2120 <100249282+Henry2120@users.noreply.github.com>
2024-01-10 21:59:05 +0700

    Update dataset.py

--- garbagedataloader/dataset.py
+++ garbagedataloader/dataset.py
@@ -7,7 +7,7 @@ import tensorflow_datasets as tfds
 
 import pathlib
 
-dataset_url = "http://clouds.iec-uit.com/smartbin.dataset/Datasets_test2.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=7MLCQ3OIV5012VWFPS9B%2F20240110%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240110T021101Z&X-Amz-Expires=604800&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiI3TUxDUTNPSVY1MDEyVldGUFM5QiIsImV4cCI6MTcwNDg5NTgzMiwicGFyZW50Ijoic21hcnRiaW4ifQ.9BPzedddwL9kZ6pqRhZUrrSXH619EB2XEW1HS2zHEL4-n6vvqv5ee-3_AyAA0_o5SWt8_tom4w32_J8i7Z_TNA&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=454bfbd3436c71b432193f436f91b45b62ba625edab1adc789cf2619a2a4b76d"
+dataset_url = "http://clouds.iec-uit.com/smartbin.dataset/Datasets_test2.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0LL7CN2X7HRX7JBTGWLN%2F20240110%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240110T145840Z&X-Amz-Expires=604800&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiIwTEw3Q04yWDdIUlg3SkJUR1dMTiIsImV4cCI6MTcwNDk0MTkxNSwicGFyZW50Ijoic21hcnRiaW4ifQ.FCx85mCiSDRx82R1JQXHw3WukfZd1CtyeCqGpSJ7pYd8bvY-2LmTusaZg_QXR7jzjAIoxXF4tSt5R-_MpG67Eg&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=9ca1b4093115b9d2195223833d263bd026e048c3fc39823b835d9ee093148eee"
 archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
 
 # Dataset path
