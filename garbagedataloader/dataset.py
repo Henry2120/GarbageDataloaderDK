2d4c1fa3c9c626c19e7b589f7fc0642bdb7f18fb
Henry2120 <100249282+Henry2120@users.noreply.github.com>
2024-01-16 15:28:35 +0700

    Update dataset.py

--- garbagedataloader/dataset.py
+++ garbagedataloader/dataset.py
@@ -7,7 +7,7 @@ import tensorflow_datasets as tfds
 
 import pathlib
 
-dataset_url = "http://clouds.iec-uit.com/smartbin.dataset/Datasets_test2.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0LL7CN2X7HRX7JBTGWLN%2F20240110%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240110T150711Z&X-Amz-Expires=604800&X-Amz-Security-Token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiIwTEw3Q04yWDdIUlg3SkJUR1dMTiIsImV4cCI6MTcwNDk0MTkxNSwicGFyZW50Ijoic21hcnRiaW4ifQ.FCx85mCiSDRx82R1JQXHw3WukfZd1CtyeCqGpSJ7pYd8bvY-2LmTusaZg_QXR7jzjAIoxXF4tSt5R-_MpG67Eg&X-Amz-SignedHeaders=host&versionId=null&X-Amz-Signature=97dc7bc3471ab6eed5e608a7b5b1d7bd8c1ef36fa396dd33e66b9d6f78ce54f4"
+dataset_url = "smartbin.dataset/Datasets_test2.zip"
 archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
 
 # Dataset path
