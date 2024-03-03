0c2ca8f79613add22797714314bc3e2d457dafbc
DESKTOP-C5N9CU6\ADMIN <vandat8080@gmail.com>
2024-03-03 09:51:02 +0700

    no message

--- garbagedataloader/dataset.py
+++ garbagedataloader/dataset.py
@@ -80,6 +80,8 @@ def val_ds(dataset_path=archive, validation_split=0.2, subset="validation", seed
     )
   return val_ds
 
+def get_classnames():
+  return train_ds.class_names
 
 # Function to print out samples 
 def get_images_using_matplotlib(dataset, class_names):
